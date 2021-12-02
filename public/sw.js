8154ab694f91e5c6741352fe09758b33518fd545
Anas Raza <anus.raza987@gmail.com>
2021-12-02 18:29:21 +0500

    make offline availability

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ const cacheName = "learn-pwa";
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll([
+      return cache.addAll([ // files that runs in offline mode
         "/static/js/vendors~main.chunk.js",
         "/static/js/main.chunk.js",
         "/static/js/bundle.js",
@@ -13,20 +13,29 @@ self.addEventListener("install", (e) => {
         "/logo192.png",
         "/favicon.ico",
         "/index.html",
+        "/user",
+        "/about",
         "/",
-        "/user"
       ]);
     })
   );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      if (res) {
-        return res;
-      }
-      fetch(e.request);
-    })
-  )
+  if (!navigator.onLine) { // if internet is not available
+    e.respondWith(
+      caches.match(e.request).then((res) => {
+        if (res) {
+          return res;
+        } else {
+          const requestUrl = e.request.clone()
+          fetch(requestUrl).then((res) => {
+            return res.json().then((result) => {
+              return result;
+            });
+          });
+        }
+      })
+    )
+  }
 })
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,25 +4,38 @@ import { Table } from 'react-bootstrap';
 const User = () => {
 
   const [data, setData] = useState([]);
+  const [mode, setMode] = useState("online");
 
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/users";
     fetch(url).then((res) => {
       res.json().then((result) => {
         setData(result);
+        localStorage.setItem("users", JSON.stringify(result));
       })
+    }).catch(() => {
+      const collection = localStorage.getItem("users");
+      setData(JSON.parse(collection));
+      setMode("offline");
+      // alert("Catch Block");
     })
   }, []);
 
   return (
     <div>
-      <Table striped bordered hover className="my-3">
+      {
+        mode === "offline" ?
+          <div className="alert alert-warning text-center" role="alert">
+            you are in offline mode or some issue with your connection
+          </div> : null
+      }
+      <Table striped bordered hover className="my-3 text-center">
         <thead>
           <tr>
             <th>#</th>
             <th>Name</th>
             <th>Username</th>
-            <th>Email</th>
+            {/* <th>Email</th> */}
           </tr>
         </thead>
         <tbody>
@@ -32,7 +45,7 @@ const User = () => {
                 <td>{obj.id}</td>
                 <td>{obj.name}</td>
                 <td>{obj.username}</td>
-                <td>{obj.email}</td>
+                {/* <td>{obj.email}</td> */}
               </tr>
             )
           })}
